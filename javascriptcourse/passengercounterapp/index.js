fe32d8cba25e56ac5d3834896cf5ada99ec2212e
Geedhavarshini <geedhavarshinii@gmail.com>
2023-10-05 22:34:02 +0530

    passenger counter app

---
+++ javascriptcourse/passengercounterapp/index.js
@@ -0,0 +1,20 @@
+//cmd+k+c for commenting multiple lines at once
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+function increment() {
+    count = count+1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+console.log("Let's count people on the subway!")
