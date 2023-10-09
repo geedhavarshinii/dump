14849daa703a9fa61e2dc5c7d7e993692596c843
Geedhavarshini <geedhavarshinii@gmail.com>
2023-10-09 23:45:37 +0530

    linkmarker - in progress

--- javascriptcourse/chromeextension/index.js
+++ javascriptcourse/chromeextension/index.js
@@ -1,4 +1,28 @@
-let inputBtn = document.getElementById("input-btn")
-inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
-})
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+let inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+inputBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += ` 
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `;
+  }
+
+  ulEl.innerHTML = listItems;
+}
