0228c01dd95282fdcf5036385c241925d6e84a94
Geedhavarshini <geedhavarshinii@gmail.com>
2023-10-09 19:57:00 +0530

    rock paper scissors - in progress

--- javascriptcourse/rockpaperscissors/index.js
+++ javascriptcourse/rockpaperscissors/index.js
@@ -1,11 +1,28 @@
-let hands = ['Rock', 'Paper', 'Scissors']
+let hands = ['rock', 'paper', 'scissors']
 let resultEl = document.getElementById("result-el")
-
-function startGame() {
-    let randomIndex = Math.floor(Math.random() * 3)
-    let result = hands[randomIndex]
-    resultEl.textContent = "Result: " + result
+let messageEl = document.getElementById("message-el")
+let oppEl = document.getElementById("opp-el")
+let winner = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
 }
+
+messageEl.textContent = "You: Rock - Paper - Scissors"
+
+function startGame(e) {
+    let randomIndex = Math.floor(Math.random() * 3)
+    let opp = hands[randomIndex]
+    oppEl.textContent = "Opponent: " + opp
+    if (opp == winner[e]) {
+        resultEl.textContent = "You win!"
+    } else if (winner[opp] == e) {
+        resultEl.textContent = "You lose!"
+    } else {
+        resultEl.textContent = "It's a draw."
+    }
+}
+
 
 //function getHand() {
    //return randomIndex
