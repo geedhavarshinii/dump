644eba96dd1e90f9f9ad80cedbf26890a2f6f1ed
Geedhavarshini <geedhavarshinii@gmail.com>
2024-01-04 21:59:20 +0530

    caesar cipher in python

---
+++ ai-winter-sem-2024/caesar-cipher.py
@@ -0,0 +1,24 @@
+def encryptdecrypt(word, key):
+    enres, deres='', ''
+    for i in range(len(word)):
+        ch = word[i]
+        if ch==" ":
+            enres+=" "
+        elif (ch.isupper()):
+            enres+=chr(ord('A')+(ord(word[i])-ord('A')+key)%26);
+        else:
+            enres+=chr(ord('a')+(ord(word[i])-ord('a')+key)%26);
+    for i in range(len(enres)):
+        char = enres[i]
+        if char==" ":
+            deres += " "
+        elif (ch.isupper()):
+            deres+=chr(ord('A')+(ord(enres[i])-ord('A')-key)%26)
+        else:
+            deres+=chr(ord('a')+(ord(enres[i])-ord('a')-key)%26)
+    return enres, deres
+
+word = input()
+key = int(input())
+encrypted, decrypted = encryptdecrypt(word, key)
+print(encrypted, decrypted)
